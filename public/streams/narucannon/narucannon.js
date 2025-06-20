875342afbcfe7342d968e6ae846d0b264cdf4fb0
ibro <batonchik2004@gmail.com>
2025-06-20 02:35:06 +0200

    8

---
+++ public/streams/narucannon/narucannon.js
@@ -0,0 +1,69 @@
+async function searchResults(keyword) {
+    const transformedResults = {
+        title: data.title,
+        image: "",
+        href: `https://pixeldrain.net/l/dX3cF5Q3`
+    };
+    
+    console.log(`Results: ${JSON.stringify(transformedResults)}`);
+    return JSON.stringify(transformedResults);
+}
+
+async function extractDetails(url) {
+    const match = url.match(/https:\/\/pixeldrain\.net\/l\/([^\/]+)/);
+    if (!match) throw new Error("Invalid URL format");
+            
+    const arcId = match[1];
+
+    const response = await soraFetch(`https://pixeldrain.net/api/list/${arcId}`);
+    const data = await response.json();    
+
+    const transformedResults = [{
+        description: `Title: ${data.title}\nFile Count: ${data.file_count}`,
+        aliases: `Title: ${data.title}\nFile Count: ${data.file_count}`,
+        airdate: ''
+    }];
+
+    console.log(`Details: ${JSON.stringify(transformedResults)}`);
+    return JSON.stringify(transformedResults);
+}
+
+async function extractEpisodes(url) {
+    const match = url.match(/https:\/\/pixeldrain\.net\/l\/([^\/]+)/);
+    if (!match) throw new Error("Invalid URL format");
+            
+    const arcId = match[1];
+
+    const response = await soraFetch(`https://pixeldrain.net/api/list/${arcId}`);
+    const data = await response.json();
+
+    const transformedResults = data.files.map((result, index) => {
+        return {
+            href: `${result.id}`,
+            number: index + 1,
+        };
+    });
+
+    console.log(`Episodes: ${JSON.stringify(transformedResults)}`);
+    return JSON.stringify(transformedResults);
+}
+
+// searchResults("all");
+// extractDetails("https://pixeldrain.net/l/sT25hhHR");
+// extractEpisodes("https://pixeldrain.net/l/sT25hhHR");
+
+async function extractStreamUrl(url) {
+    return `https://pixeldrain.net/api/file/${url}`;
+}
+
+async function soraFetch(url, options = { headers: {}, method: 'GET', body: null }) {
+    try {
+        return await fetchv2(url, options.headers ?? {}, options.method ?? 'GET', options.body ?? null);
+    } catch(e) {
+        try {
+            return await fetch(url, options);
+        } catch(error) {
+            return null;
+        }
+    }
+}
